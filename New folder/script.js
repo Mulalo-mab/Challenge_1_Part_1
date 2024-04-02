34dc357a0add6986fcdfcfba18d3caaa03a7103f
Mulalo <mulalomabuda@gmail.com>
2024-04-02 13:18:12 +0200

    Comments
    I added comments to script.js

--- New folder/script.js
+++ New folder/script.js
@@ -1,13 +1,20 @@
-let firstName = 'John';
-let age = 35;
-let hobbyName = 'Coding'; // Changed variable name to avoid conflict
+let firstName = 'John'; // Declaring a variable for first name
+let age = 35; // Declaring a variable for age
+let hobbyName = 'Coding'; // Declaring a variable for hobby
 
+// Defining an arrow function to log a message twice
 const logTwice = () => {  
-  return `Hello, ${name} (${age}). I love ${hobbyName}!`
+  // Within this function, there's an attempt to access variables 'name' and 'hobbyName',
+  // which are not defined in the scope of this function. Instead, it should access 'firstName' and 'hobbyName'.
+  return `Hello, ${firstName} (${age}). I love ${hobbyName}!`
 }
 
+// This line attempts to log a message twice using template literals
+// However, it's outside any function, so it will result in an error.
 const twice = `Hello, ${name} (${age}). I love ${hobbyName}!`
-console.log(twice);
-console.log(twice);
-logTwice();
+
+console.log(twice); // This line will throw an error due to the use of 'name' variable, which is not defined
+console.log(twice); // This line will also throw an error
+
+logTwice(); // This line will execute the logTwice function but it doesn't log anything as the return value isn't used
 
